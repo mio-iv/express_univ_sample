2b967fe8943efd11f20a6cc969433fd139cfb130
飯田菜摘 <natsumi@iidanatsuminoMacBook-Air.local>
2024-05-14 19:04:28 +0900

    helloページ追加

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//設定ファイル(require()で設定の読み込みをする模様)
+var createError = require('http-errors'); //HTTPエラーの対処を行うもの
+var express = require('express');         //express本体
+var path = require('path');               //ファイルパスを扱う
+var cookieParser = require('cookie-parser');  //クッキーのパース（値の変換）
+var logger = require('morgan');           //HTTPリクエストのログ出力
 
-var indexRouter = require('./routes/index');
+//ルート用モジュールのロード
+var indexRouter = require('./routes/index');  //index.jsは/indexにアクセスしたときの処理
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 
+//Expressのオブジェクトを作成
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//関数の組み込み（上記でロードしたモジュールの機能を呼び出せるようにしている）
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ルート用、エラー用のapp.use
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
